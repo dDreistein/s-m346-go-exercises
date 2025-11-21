e66ad12bf3fce41d831d67500cbcbb6d38008d89
dDreistein <178164238+dDreistein@users.noreply.github.com>
2025-11-21 12:21:10 +0100

    1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,14 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	var firstName string = "Bela"
+	var lastName string = "Müller"
+	var dayOfBirth int = 2
+	var monthOfBirth int = 6
+	var yearOfBirth int = 2008
+	var numberOfSiblings int = 1
+	var heightInMeters float32 = 1.69
+	var zodiacSign byte = 'Z'
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
