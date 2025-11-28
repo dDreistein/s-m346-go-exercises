b54d9414c4068392210caed976710862c6b70f6d
dDreistein <178164238+dDreistein@users.noreply.github.com>
2025-11-28 12:07:27 +0100

    2.3

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -3,14 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: create a map called "modules"
-
+	modules := map[int]string{
+		104: "Datenmodell implementieren",
+		117: "Informatik- und Netzinfrastruktur für ein kleines Unternehmen realisieren",
+		346: "Cloud Lösungen konzipieren und realisieren",
+	}
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
-	// TODO: delete one
-	// TODO: add one
-	// TODO: replace one
+	delete(modules, 117)
+	modules[161] = "Daten analysieren und modellieren"
+	modules[161] = "Standortgebundene Kommunikationsdienste betreiben"
 	fmt.Println(modules)
 }
