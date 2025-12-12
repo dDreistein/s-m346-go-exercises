15eb9a4b1ff66c26f7db6007e58ea549615b58e0
dDreistein <178164238+dDreistein@users.noreply.github.com>
2025-12-12 12:16:56 +0100

    4.2

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,16 @@
 package main
 
-// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+import (
+	"fmt"
+	"math"
+)
+
+func computeHypotenuse(a float64, b float64) float64 {
+	return math.Sqrt(a*a + b*b) // Ist übersichtlicher als math.Pow()
+}
 
 func main() {
-	// TODO: call the function computeHypotenuse
+	fmt.Println(computeHypotenuse(1.0, 2.0))       // 2.23606797749979
+	fmt.Println(computeHypotenuse(5, 12.0))        // 13
+	fmt.Println(computeHypotenuse(3.2345, 4.6789)) // 5.688066056226844
 }
