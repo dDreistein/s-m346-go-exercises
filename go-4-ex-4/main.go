6f0ffdaa516fee55f54afc0ec069fd32bffa9a24
dDreistein <178164238+dDreistein@users.noreply.github.com>
2025-12-12 12:41:18 +0100

    4.4

--- go-4-ex-4/main.go
+++ go-4-ex-4/main.go
@@ -1,9 +1,22 @@
 package main
 
-// TODO: implement the function convertCelsiusToFahrenheit
-// TODO: implement the function convertFahrenheitToCelsius
+import "fmt"
+
+func convertCelsiusToFahrenheit(celsius float64) float64 {
+	return (celsius * 9 / 5) + 32
+}
+
+func convertFahrenheitToCelsius(fahrenheit float64) float64 {
+	return (fahrenheit - 32) * 5 / 9
+}
 
 func main() {
-	// TODO: call the function convertCelsiusToFahrenheit
-	// TODO: call the function convertFahrenheitToCelsius
+	fmt.Println(convertCelsiusToFahrenheit(273.15)) // 523.67
+	fmt.Println(convertCelsiusToFahrenheit(0))      // 32
+	fmt.Println(convertCelsiusToFahrenheit(100))    // 212
+
+	fmt.Println(convertFahrenheitToCelsius(32))     // 0
+	fmt.Println(convertFahrenheitToCelsius(212))    // 100
+	fmt.Println(convertFahrenheitToCelsius(491.67)) // 255.3722222222222
+
 }
